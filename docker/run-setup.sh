4105f41275c4915f415fc7386aefbb5e04726386
Cole <cole@ubuntu2004.linuxvmimages.local>
2021-03-29 22:50:50 -0400

    client works

--- docker/run-setup.sh
+++ docker/run-setup.sh
@@ -16,7 +16,7 @@ DEFAULT_CONTENT_LIFETIME=${DEFAULT_CONTENT_LIFETIME:-"7200"}
 HICN_MTU=${HICN_MTU:-"1300"}
 FIRST_IPV6_WORD=${FIRST_IPV6_WORD:-"b001"}
 USE_MANIFEST=${USE_MANIFEST:-"true"}
-HICN_PREFIX=${HICN_PREFIX:-"http://hicn-http-proxy "}
+HICN_PREFIX=${HICN_PREFIX:-"http://hicn-http-proxy"}
 UDP_TUNNEL_ENDPOINTS=${UDP_TUNNEL_ENDPOINTS:-"198.111.224.199:33567"}
 HICN_SERVER=${HICN_SERVER:-"198.111.224.199"} 
 NETWORK_INTERFACE=${NETWORK_INTERFACE:-"eth0"}
@@ -43,12 +43,16 @@ iptables -t nat -A POSTROUTING -j MASQUERADE \
   -o ${NETWORK_INTERFACE}
   
 echo "Setting up client..."
+# Masquerade all the traffic coming from VPP
+iptables -t nat -A POSTROUTING -j MASQUERADE \
+  --src ${TAP_ADDRESS_NET} ! --dst ${TAP_ADDRESS_NET} -o ${NETWORK_INTERFACE}
+
 # Add default route to vpp
 vppctl ip route add 0.0.0.0/0 via ${TAP_ADDRESS_KER} ${TAP_NAME}
+
 # Set UDP punting
 vppctl hicn punting add prefix ${FIRST_IPV6_WORD}::/16 \
-  intfc ${TAP_NAME} type udp4 \
-  dst_port ${HICN_LISTENER_PORT}
+  intfc ${TAP_NAME} type udp4 dst_port ${HICN_LISTENER_PORT}
 
 # Face route and punting
 vppctl hicn face udp add \
@@ -72,8 +76,6 @@ iptables -A FORWARD \
   --out-interface ${NETWORK_DEVICE} \
   -j ACCEPT
   
-iptables -t nat -A POSTROUTING --out-interface tun0 -j MASQUERADE
+iptables -t nat -A POSTROUTING --out-interface ${NETWORK_DEVICE} -j MASQUERADE
 
-# Prevents container from exiting at completion
-echo "Done! Exiting..."
-bash
+echo "Done! Returning..."
